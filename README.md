ad1ff00167512e4d92c7d10824fe2a036dcd8370
Vilate <viknapp22@gmail.com>
2024-10-05 15:27:41 -0600

    read and write data implemented

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 {Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
 
-This is the lowest level language in which I've developed a project. I think when you learn the lower level languages, it helps you further your abilities in higher level languages because you understand better how the computer is thinking.
+This is the lowest level language in which I've developed a project. I think when you learn the lower level languages, it helps you further your abilities in higher level languages because you understand better how the computer is thinking. There where two primary purposes for building this software. The first was to explore C++ and become more comfortable in lower level syntax. The second was to develop and demonstrate competency with json data, loading, and saving data to a file from C++.
 
 {Provide a description of the software that you wrote to demonstrate the C++ language.}
 
@@ -22,13 +22,16 @@ This is the lowest level language in which I've developed a project. I think whe
 
 {Make a list of websites that you found helpful in this project}
 
-- [Web Site Name](http://url.link.goes.here)
-- [Web Site Name](http://url.link.goes.here)
+- [Map in C++ YouTube Video with practical examples](https://youtu.be/aEgG4pidcKU?si=wtvlhpkCzXGHlF-R)
+- [C++ Maps YouTube Video](https://youtu.be/agDPYyTsAH4?si=dPv9xq-U_O8_ngPb)
+- [Effortless JSON in C++](https://youtu.be/3XubaLCDYOM?si=4XPouL7lFrsbplg7)
+- [C++ Tutorial for Beginners](https://youtu.be/S3nx34WFXjI?si=Igv3Rb2NZrfbuM0K)
+- [Stack Overflow](https://stackoverflow.com/questions/454762/vector-or-map-which-one-to-use)
 
 # Future Work
 
 {Make a list of things that you need to fix, improve, and add in the future.}
 
-- Item 1
-- Item 2
+- Add Credit Amounts and display them
+- Include Possible Course Codes with Classes
 - Item 3
--- main.cpp
+++ main.cpp
@@ -1,21 +1,94 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
+#include <fstream>
+#include "include/json.hpp"
 
+// shortcut to not add std in the code
 using namespace std;
+using json = nlohmann::json;
 
-void studentMap()
+struct Assignment
 {
+    string assignment;
+    int grade;
+};
+
+struct Class
+{
+    string className;
+    string teacher;
+    vector<Assignment> assignments;
+};
+
+struct Student
+{
+    string name;
+    double gpa;
+    vector<Class> classes;
+};
+
+// map<string, Student> studentMap();
+void viewInfo(const map<string, Student>& studentData);
+void modifyStudentData(map<string, Student>& students);
+void updateStudentGrade();
+void createAssignment();
+void calculateGPA();
+void exitProgram();
+void writeDataToFile(const map<string, Student>& students);
+
+
+map<string, Student> studentMap()
+{
+    map<string, Student> students;
     // load JSON file that is a map of students with additional data
+    // open file
+    ifstream inputFile("studentData.json");
+
+    // checks for file not being able to open
+    if (!inputFile.is_open()) {
+        cerr << "File can't be opened." << endl;
+        // return empty map
+        return students;
+    }
+
+    json j;
+    inputFile >> j;
+
+    for (const auto& studentData : j["students"]) {
+        Student student;
+        student.name = studentData["name"];
+        student.gpa = studentData["gpa"];
+
+        for (const auto& classData : studentData["classes"]) {
+            Class classInfo;
+            classInfo.className = classData["className"];
+            classInfo.teacher = classData["teacher"];
+
+            for (const auto& assignmentData : classData["assignments"]) {
+                Assignment assignment;
+                assignment.assignment = assignmentData["assignment"];
+                assignment.grade = assignmentData["grade"];
+                classInfo.assignments.push_back(assignment);
+            }
+            student.classes.push_back(classInfo);
+        }
+        students[student.name] = student;
+    }
+
+    //close the file
+    inputFile.close();
 
     // return the map
-
+    return students;
 }
 
 void menu()
 {
     // give a list of options for the user
     // menu should include:
+    map<string, Student> students = studentMap();
     int choice;
 
     do
@@ -39,7 +112,7 @@ void menu()
             a. view total students with names and gpa
             b. view student by name with detailed info
             */
-            viewInfo();
+            viewInfo(students);
             break;
         case 2:
             /*
@@ -47,7 +120,7 @@ void menu()
                 a. (takes name input and adds a spot in the json file)
                 b. or delete student
             */
-            modifyStudentData();
+            modifyStudentData(students);
             break;
 
         case 3:
@@ -79,14 +152,84 @@ void menu()
     } while (choice != 6); 
 }
 
-void viewInfo()
+void viewInfo(const map<string, Student>& studentData)
 {
+    int choice;
+    cout << "1. View all students" << endl;
+    cout << "2. View individual student" << endl;
+    cout << "Enter your choice: ";
 
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "All Students:" << endl;
+        for (const auto& student : studentData) {
+            string name = student.first;
+            double gpa = student.second.gpa;
+            cout << "Name: " << name << "......." << "GPA: " << gpa << endl;
+        }
+    } else if (choice == 2) {
+        cout << "Enter the student's name: ";
+        string studentName;
+        cin.ignore();
+        getline(cin, studentName);
+
+        if (studentData.find(studentName) != studentData.end()) {
+            const Student& studentInfo = studentData.at(studentName);
+
+            cout << studentInfo.name << endl;
+            cout << "GPA: " << studentInfo.gpa << endl;
+            cout << "Class Info:" << endl;
+
+            for (const auto& course : studentInfo.classes) {
+                cout << course.className << "|" << course.teacher << endl;
+                cout << "Assignments: " << endl;
+
+                for (const auto& assignment : course.assignments) {
+                    cout << " > " << assignment.assignment << "........" << assignment.grade << endl;
+                }
+            }
+        } else {
+            cout << "The student could not be found." << endl;
+        }
+    } else {
+        cout << "That is not a valid choice! Try again." << endl;
+    }
 }
 
-void modifyStudentData()
+void modifyStudentData(map<string, Student>& students)
 {
+    int choice;
+    cout << "1. Add a new student" << endl;
+    cout << "2. Delete a student" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
 
+    if (choice == 1) {
+        Student newStudent;
+        cout << "Enter the student's name: ";
+        cin.ignore();
+        getline(cin, newStudent.name);
+        cout << "Enter student's GPA: ";
+        cin >> newStudent.gpa;
+
+        students[newStudent.name] = newStudent;
+        cout << "Student added!" << endl;
+    } else if (choice == 2) {
+        cout << "Enter the name of the student to delete: ";
+        string studentName;
+        cin.ignore();
+        getline(cin, studentName);
+
+        if (students.erase(studentName)) {
+            cout << "Student has been deleted!" << endl;
+        } else {
+            cout << "Student could not be found." << endl;
+        }
+    } else {
+        cout << "Invalid choice! Try again." << endl;
+    }
+    writeDataToFile(students);
 }
 
 void updateStudentGrade()
@@ -109,7 +252,44 @@ void exitProgram()
 
 }
 
+void writeDataToFile(const map<string, Student>& students)
+{
+    json j;
+    j["students"] = json::object();
+
+    for (const auto& [name, student] : students) {
+        json studentJson;
+        studentJson["name"] = student.name;
+        studentJson["gpa"] = student.gpa;
+
+        studentJson["classes"] = json::object();
+        for (const auto& course : student.classes) {
+            json classJson;
+            classJson["className"] = course.className;
+            classJson["teacher"] = course.teacher;
+
+            classJson["assignments"] = json::array();
+            for (const auto& assignment : course.assignments) {
+                json assignmentJson;
+                assignmentJson["assignment"] = assignment.assignment;
+                assignmentJson["grade"] = assignment.grade;
+                classJson["assignments"].push_back(assignmentJson);
+            }
+            studentJson["classes"][course.className] = classJson;
+        }
+        j["students"][name] = studentJson;
+    }
+    ofstream outputFile("studentData.json");
+    if (!outputFile.is_open()) {
+        cerr << "Cannot open file for writing!" << endl;
+        return;
+    }
+    outputFile << setw(4) << j << endl;
+    outputFile.close();
+}
+
 int main()
 {
+    menu();
     return 0;
 }
